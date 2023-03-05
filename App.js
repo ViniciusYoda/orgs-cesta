8518b85ab5a8643b711943f4654dd67a9899dd29
Vinícius Yoda <vyoda4604@gmail.com>
2023-03-05 08:55:58 -0300

    image

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { StatusBar, SafeAreaView View } from 'react-native';
+import { StatusBar, SafeAreaView, View } from 'react-native';
 
 import Cesta from './src/telas/Cesta';
 
 export default function App() {
   return (
-    <SafeAreaVie>
+    <SafeAreaView>
       <StatusBar />
       <Cesta />
-    </SafeAreaVie>
+    </SafeAreaView>
   );
 }
 
